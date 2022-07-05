5fa39f2f13e952dfa2f552ac4f47c0cd8e4ce6aa
preeti1428 <58389536+preeti1428@users.noreply.github.com>
2022-07-06 03:22:05 +0530

    Cycle in Undirected graph
    (Using dfs/bfs both)

--- Daily progress/Graphs/cyclecheck.cpp
+++ Daily progress/Graphs/cyclecheck.cpp
@@ -1,64 +1,93 @@
-bool dfsrec(vector<int>&visited, vector<vector<int>>&B, vector<int>&recst, int s){
-    visited[s]=1;
-    recst[s] = 1;
-    
-    for(auto u : B[s]){
-        if(visited[u]==0 && dfsrec(visited,B,recst,u)) return true;
-        else if(recst[u]) return true;
+bool isCyclicBFS(int src, unordered_map<int,bool> &visited, unordered_map<int, vector<int>> &adj)
+{
+    unordered_map<int,int> parent;
+    visited[src]=1;
+    parent[src]=-1;
+    queue<int> q;
+    q.push(src);
+    while(!q.empty())
+    {
+        int front=q.front();
+        q.pop();
+        for(auto neighbour: adj[front])
+        {
+            if(visited[neighbour] && parent[front]!=neighbour)
+            {
+                return 1;
+            }
+            else if(!visited[neighbour])
+            {
+                q.push(neighbour);
+                visited[neighbour]=1;
+                parent[neighbour]=front;
+            }
+        }
     }
-    
-    recst[s]=0;
+    return 0;
+}
+
+bool dfsrec(unordered_map<int,vector<int>>adj, unordered_map<int,bool>visited, int s, int parent){
+     visited[s]= true;
+     
+     for(int u : adj[s]){
+         if(visited[u]==false){
+            bool cyc = dfsrec(adj, visited, u, s);
+            if(cyc) return 1;
+             
+         }
+         else if(u!=parent)
+         return true;
+     }
     
     return false;
 }
 
 
 int Solution::solve(int A, vector<vector<int> > &B) {
-    vector<vector<int>>adj(A+1);
     
-    for(auto i : B){
-        adj[i[0]].push_back(i[1]);
-        i.clear();
+    
+    unordered_map<int,vector<int>>adj;
+      for(int i=0;i<B.size();i++)
+    {
+        int u=B[i][0];
+        int v=B[i][1];
+       
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
+   
+    unordered_map<int,bool> visited;
+   
+    for(int i=1;i<=A;i++)
+    {
+        if(!visited[i])
+        {
+            bool ans= isCyclicBFS(i,visited,adj);
+            //bool ans=isCyclicDFS(i,-1,visited,adj);
+            if(ans==1)
+                return 1;
+        }
+    }
+    return 0;
     
-    vector<int>visited(A+1,0);
-    vector<int>recst(A+1,0);
-    
-    for(int i=1;i<=A;i++){
-        if(dfsrec(visited,adj,recst,i)) return true;
+    /*
+    unordered_map<int,bool>visited;
+    for(int i=0; i<B.size();i++){
+        int u = B[i][0];
+        int v = B[i][1];
+        
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+   
+        
+    for(int i=1; i<=A;i++){
+        if(visited[i]==false){
+        bool bol = dfsrec(adj, visited, i, -1);
+        
+        if(bol) return true;
+        }
     }
     return false;
+    */
 }
-
-/*bool dfs(int src, vector<vector<int> > &B, vector<int> &visited, vector<int> &dfsVis){
-visited[src] = 1;
-dfsVis[src] = 1;
-for(auto e: B[src]){
-if(visited[e] == 0){
-if(dfs(e, B, visited, dfsVis)){
-return true;
-}
-}else if(dfsVis[e]){
-return true;
-}
-}
-dfsVis[src] = 0;
-return false;
-}
-
-int Solution::solve(int A, vector<vector<int> > &B) {
-vector<int> visited(A+1, 0);
-vector<int> dfsVis(A+1, 0);
-vector<vector<int> > graph(A+1);
-for(auto v: B){
-graph[v[0]].push_back(v[1]);
-v.clear();
-}
-for(int i = 1; i <= A; i++){
-if(dfs(i,graph, visited, dfsVis)){
-return true;
-}
-}
-return false;
-}
-*/
